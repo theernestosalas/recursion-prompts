4ef99759b5503bd4e3efa16fa7934d689db15e76
Ernesto Salas <93291493+theernestosalas@users.noreply.github.com>
2023-08-26 10:31:23 -0600

    problem 5 done

--- src/recursion.js
+++ src/recursion.js
@@ -71,6 +71,11 @@ var isEven = function(n) {
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+    if(n <= 0) {
+        return -1;
+    }
+    return n-1 + sumBelow(n-1);
+
 };
 
 // 6. Get the integers within a range (x, y).
